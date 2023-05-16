07e6c964a5495e0b4062b276cc3a4fc5bc3b0166
mmaaxxii <maximilianoespeche@gmail.com>
2023-05-16 09:18:36 -0300

    Fixing conflic

---
+++ src/pages/Portfolio/components/carousel/styled-components/Carousel.style.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const CarouselImg = styled.img`
+    max-width: 500px;
+    width: 100%;
+    height: auto;
+    opacity: 0;
+    transition: 1s;
+    &.loaded {
+        opacity: 1;
+    }
+`
+
+export const CarouselContainer = styled.div`
+    display: flex;
+    align-content: center;
+    flex-direction: row;
+    margin-top: 15px;
+`
+
+export const CarouselButton = styled.button`
+    color: white;
+    background-color: #eb118a;
+    padding: 8px;
+    margin: 0 5px;
+`
