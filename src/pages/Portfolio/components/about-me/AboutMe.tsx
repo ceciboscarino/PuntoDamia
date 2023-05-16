d765806e97b4f47b49aa6714e76e22604111180d
mmaaxxii <maximilianoespeche@gmail.com>
2023-05-16 18:16:56 -0300

    transition.style

--- src/pages/Portfolio/components/about-me/AboutMe.tsx
+++ src/pages/Portfolio/components/about-me/AboutMe.tsx
@@ -1,19 +1,21 @@
 import { AboutMeContainer , AboutMeImg} from ".";
 import  aboutmeimage  from "./assets/ceci.jpg"
-
+import 'transition-style';
 
 
 
 function AboutMe() {
     return (
-        <AboutMeContainer id="aboutme">
-            <h1>Nosotros</h1>
-            <div>
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia sequi impedit numquam quas amet provident cum voluptatibus deserunt voluptatem enim asperiores nam ipsa, odio officia molestiae nobis ea quisquam.</p>
-                
-                <AboutMeImg  alt= "picture" src= {aboutmeimage} />
-            </div>
-        </AboutMeContainer>
+        <div transition-style="in:circle:bottom-right">
+            <AboutMeContainer id="aboutme">
+                <h1>Nosotros</h1>
+                <div>
+                    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus quia sequi impedit numquam quas amet provident cum voluptatibus deserunt voluptatem enim asperiores nam ipsa, odio officia molestiae nobis ea quisquam.</p>
+                    
+                    <AboutMeImg  alt= "picture" src= {aboutmeimage} />
+                </div>
+            </AboutMeContainer>
+        </div>
     )
 }
 export default AboutMe
